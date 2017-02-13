49756f6c08e44ec602e1311c67c4342d174b57f9
Wen Zhang <iwenyou@gmail.com>
2017-02-13 11:50:06 -0800

    added new view and modified link controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -8,6 +8,10 @@ class LinksController < ApplicationController
   end
 
   def create
-
+    @target_url = Link.create(
+    user_id: current_user.id,
+    target_url: params[:target_url],
+    slug: params[:slug]
+    )
   end
 end
