2f9b48df373c27ac879bfc7c21ade76b7862826f
Wen Zhang <iwenyou@gmail.com>
2017-02-13 19:06:33 -0800

    added visits controller and create action

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
  end
 
   resource :links
+  post "/links/:wildcard" => "visits#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
